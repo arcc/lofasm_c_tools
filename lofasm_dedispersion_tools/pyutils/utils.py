ef718f0617ab374ee7f759e613e499ae55206667
jingluo <jingluo@jingluos-MacBook-Air.local>
2017-07-19 17:07:38 -0500

    optimize config file and add functionality to write dedipserion script

--- lofasm_dedispersion_tools/pyutils/utils.py
+++ lofasm_dedispersion_tools/pyutils/utils.py
@@ -14,4 +14,21 @@ def compute_time_delay(DM, target_freq, ref_freq):
         The reference frequency that use to compute time delay.
     """
     dt = DM * DMconst*(1/target_freq**2 - 1/ref_freq**2)
-    return dt 
+    return dt
+
+def compute_min_smear_size(DM, ref_freq, sampling_freq, sampling_time):
+    """This is a function computes the minimun smear_size for one DM.
+    Parameter
+    ---------
+    DM: astropy quantity
+        The DM for checking smear size
+    ref_freq: astropy quantity
+        The reference frequency, generally the frequency has zero delay
+    sampling_freq: astropy quantity
+        Sampling freq
+    sampling_time: astropy quantity
+        Sampling time
+    """
+    dt = compute_time_delay(DM, ref_freq - sampling_freq, ref_freq)
+    size = int(dt/sampling_time)
+    return size
